ef24cf3f5a56acde3b7b0c06779b2852b68a8f16
ToTuKa <Kt08007921@gmail.com>
2026-06-19 15:49:09 +0900

    [add]ステージゲートのシステム完成 注：画面に表示される文字は画面と一緒に動くためうまいこと調整

---
+++ Assets/Script/ChcekOnObjject.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ChcekOnObjject : MonoBehaviour
+{
+    [SerializeField, Tooltip("次に移行するキャンバスを入れる")] GameObject canvas;
+
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canvas.SetActive(false);
+        }
+    }
+
+}
